695083938bd2e612fecfc6a62ab3404c1cf87c2c
Ritik Yaduvanshi <yadavritik467@gmail.com>
2025-06-10 13:04:32 +0530

    disbaled login/signup button

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -135,7 +135,9 @@ const Login = () => {
             {/* Submit Button */}
             <button
               type="submit"
-              className="w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold py-3 px-6 rounded-xl hover:from-purple-600 hover:to-pink-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-transparent transition-all duration-300 transform hover:scale-105 shadow-lg flex items-center justify-center gap-2"
+              disabled={loading}
+
+              className={`${loading ? "opacity-40":""} w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold py-3 px-6 rounded-xl hover:from-purple-600 hover:to-pink-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-transparent transition-all duration-300 transform hover:scale-105 shadow-lg flex items-center justify-center gap-2`}
             >
               <Sparkles className="w-5 h-5" />
               {loading
